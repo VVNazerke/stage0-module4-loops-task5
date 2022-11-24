109ee562e1907d33ca7b33f29a152e985f3c9d93
admin <nbisametova@mail.ru>
2022-11-25 02:22:02 +0600

    1st try

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,23 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        for (int i = 0; i < height; i++) {
+            if (i<height/2) {
+                for (int j = 0; j < height; j++) {
+                    if (j >= i && j <= height - (i + 1))
+                        System.out.print(8);
+                    else
+                        System.out.print(" ");
+                }
+            } else{
+                for (int j = height; j >0; j--) {
+                    if (j > height - (i + 1) && j <= i+1)
+                        System.out.print(8);
+                    else
+                        System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
